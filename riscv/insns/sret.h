c3e0b1f5d93dcd5879292ad0b2d2bde3f95c5e99
Unik-lif <songlinke18@mails.ucas.ac.cn>
2026-09-26 00:38:19 +0800

    Enforce trusted AS entry on SRET

--- riscv/insns/sret.h
+++ riscv/insns/sret.h
@@ -7,11 +7,19 @@ if (STATE.v) {
 } else {
   require_privilege(get_field(STATE.mstatus->read(), MSTATUS_TSR) ? PRV_M : PRV_S);
 }
-reg_t next_pc = p->get_state()->sepc->read();
+const bool enter_as = get_field(STATE.asstatus->read(), NACC_ASSTATUS_SPA);
+if (enter_as) {
+  require(get_field(STATE.sstatus->read(), MSTATUS_SPP) == PRV_S);
+  require(get_field(STATE.satp->read(), SATP64_MODE) == SATP_MODE_SV39);
+  require((STATE.astvec->read() & ~reg_t(3)) != 0);
+  // An untrusted S context must not carry stale translations into AS.
+  MMU.flush_tlb("as-entry");
+}
+reg_t next_pc = enter_as ? STATE.astvec->read() & ~reg_t(3) : STATE.sepc->read();
 set_pc_and_serialize(next_pc);
 reg_t s = STATE.sstatus->read();
 reg_t prev_prv = get_field(s, MSTATUS_SPP);
-s = set_field(s, MSTATUS_SIE, get_field(s, MSTATUS_SPIE));
+s = set_field(s, MSTATUS_SIE, enter_as ? 0 : get_field(s, MSTATUS_SPIE));
 s = set_field(s, MSTATUS_SPIE, 1);
 s = set_field(s, MSTATUS_SPP, PRV_U);
 bool prev_virt = STATE.v;
@@ -48,5 +56,11 @@ STATE.sstatus->write(s);
 reg_t as = STATE.asstatus->read();
 STATE.nacc_a = get_field(as, NACC_ASSTATUS_SPA);
 as = set_field(as, NACC_ASSTATUS_SPA, 0);
+if (enter_as) {
+  // Record the actual S origin; leave the saved AU EPC/cause/tval alone.
+  as &= ~(NACC_ASSTATUS_ASPA | NACC_ASSTATUS_ASPP |
+          NACC_ASSTATUS_ASPIE | NACC_ASSTATUS_ASIE);
+  as |= NACC_ASSTATUS_ASPP;
+}
 STATE.asstatus->write(as);
 p->set_privilege(prev_prv, prev_virt);
